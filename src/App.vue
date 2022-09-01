86b2a99c479dd0f19a2418da69f8326b607b2a5f
briangor <briangor13@gmail.com>
2022-09-01 15:51:24 +0300

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,12 @@
 <template>
   <div id="app">
-  
-    {{ usersData }}
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import axios from 'axios'
-
-const url = 'https://reqres.in/api/users?page=1'
-
-export default {
-  name: 'App',
-  data() {
-    return {
-      alldata: null,
-      usersData: null,
-    }
-  },
-  components: {
-
-  },
-  methods: {
-  
-  },
-  async created() {
-    console.log("mounted");
-    try {
-      const res = await axios.get(url)
-      this.alldata = res.data
-      this.usersData = this.alldata.data
-      console.log(res)
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
-</script>
 
 <style>
 #app {
@@ -45,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
