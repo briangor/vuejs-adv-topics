9e330403a9e76c0dd31a7e8eb76260e0836f8bc1
briangor <briangor13@gmail.com>
2022-09-01 15:49:13 +0300

    add HomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+  {{ usersData}}
+  <Footer></Footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios'
+import Footer from '@/components/Footer.vue';
+
+const url = 'https://reqres.in/api/users?page=1'
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      alldata: null,
+      usersData: null,
+    }
+  },
+  components: {
+    Footer
+},
+  async created() {
+    console.log("mounted");
+    try {
+      const res = await axios.get(url)
+      this.alldata = res.data
+      this.usersData = this.alldata.data
+      console.log(res)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+</script>
