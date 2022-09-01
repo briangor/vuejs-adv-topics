e921c0359cb150e443011d6ae440578aa0d4363c
briangor <briangor13@gmail.com>
2022-09-01 17:02:09 +0300

    add StudentView

---
+++ src/views/StudentView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>
+            student
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'StudentView',
+    watch: {
+        // $route(to, from) {
+        //     // react to route changes...
+        // }
+    }
+}
+</script>
