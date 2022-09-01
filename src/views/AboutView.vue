7563b67e919655c0943a51c3119cfe0fe0834184
briangor <briangor13@gmail.com>
2022-09-01 15:49:34 +0300

    add AboutView

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h5>About VueJS School</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut odio dictum, porta lorem sit amet, sagittis nisl. Quisque sed sem eu quam iaculis ultricies id vitae ante. Cras lobortis tempus massa vel varius. Nullam pharetra tristique mauris. Morbi suscipit metus orci, quis hendrerit metus mollis at. Sed molestie neque ut elit finibus tempor. Morbi vel erat nisl. Maecenas in nisi eget est elementum congue. Pellentesque sit amet diam lectus. Vestibulum lobortis massa eget mauris tempor, vel varius mauris cursus. Sed facilisis, lorem a aliquet aliquam, sapien erat tristique risus, ut fringilla quam arcu eu libero. Praesent molestie, orci laoreet mattis sagittis, magna ex commodo sapien, et ultricies nibh mi non mauris. Integer quam urna, fringilla dignissim pellentesque blandit, viverra eu dui. Mauris interdum ligula sodales enim feugiat ultrices et in lorem.</p>
+    <p>Curabitur in nisl sagittis, eleifend mauris eu, rhoncus nisi. Sed eget felis posuere, faucibus eros vel, vestibulum dui. Curabitur quis nulla eget est pulvinar porta ac ac mauris. Vivamus sed nisl mi. Quisque ut ex et lorem ultrices convallis. Nulla mollis aliquet nisi. Curabitur dictum sed ipsum eget bibendum. Suspendisse sed sollicitudin mauris. Vestibulum ut justo eget justo hendrerit auctor id vitae arcu. Nunc lacinia metus nisi, ac hendrerit dui placerat sit amet. Sed ac quam tellus. Mauris rhoncus, nunc id interdum scelerisque, lacus massa ornare nisi, eget blandit tortor dui nec nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p>
+
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+import Footer from '@/components/Footer.vue';
+export default {
+    name: "AboutView",
+    components: { Footer }
+}
+</script>
