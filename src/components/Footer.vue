1c6c77fc3d7fc6d59dc42e8ec5ca7386674bdb46
briangor <briangor13@gmail.com>
2022-09-01 15:50:13 +0300

    add Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="footer">
+        <p>Legal Stuff | Privacy Policy</p>
+        <p>© VueJS School 2022s</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'FooterComponent'
+}
+</script>
