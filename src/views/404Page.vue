35aa0be69adf3c9f257eab353232aa4e57ee2b19
briangor <briangor13@gmail.com>
2022-09-01 17:02:18 +0300

    add 404 page

---
+++ src/views/404Page.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="error-page">
+        <h2>404</h2>
+        <h3>Page Not Found</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PageView404'
+}
+</script>
